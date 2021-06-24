3c2e750f029dcf678d1c3b036b3a2ba2f74a1ee6
VikasVerma25 <vikasverma250999@gmail.com>
2021-06-24 23:05:43 +0530

    statistics

--- String/Day2/Get_Statistics.py
+++ String/Day2/Get_Statistics.py
@@ -27,20 +27,20 @@ def stats(inputList):
     answer = {'mean':None,"median":None,"mode":None,
         "sample_variance":None,"sample_standerd_deviation":None,
         "mean_confidence_interval":None}
-    mean = answer["mean"] = sum(inputList)/inputlen
+    mean = answer["mean"] = round(sum(inputList)/inputlen, 4)
     modedict = dict()
     squaresum = 0
     for n in inputList:
         squaresum += (n - mean)**2
         modedict[n] = 1 if n not in modedict else modedict[n]+1
-    answer["sample_variance"] = squaresum/(inputlen-1)
-    answer["sample_standerd_deviation"] = answer["sample_variance"]**0.5
+    answer["sample_variance"] = round(squaresum/(inputlen-1), 4)
+    sd = answer["sample_standerd_deviation"] = round(answer["sample_variance"]**0.5, 4)
     max = modedict[inputList[0]]
     for i in modedict:
         if modedict[i] > max:
             max = modedict[i]
             answer["mode"] = i
-    answer["median"] = (answer["mode"] + 2*answer["mean"])/3
-
+    answer["median"] = round((answer["mode"] + 2*answer["mean"])/3, 4)
+    answer["mean_confidence_interval"] = [round(mean-1.96*sd/(inputlen**0.5),4),round(mean+1.96*sd/(inputlen**0.5),4)]
     return answer
 print(stats([2,1,3,4,4,5,6,7]))
