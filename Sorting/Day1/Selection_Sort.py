0bafcfc5c43ca51c5d10dfac82abd52c9b576326
VikasVerma25 <vikasverma250999@gmail.com>
2021-07-22 23:37:05 +0530

    selection

--- Sorting/Day1/Selection_Sort.py
+++ Sorting/Day1/Selection_Sort.py
@@ -1,4 +1,4 @@
-def insertion(a, n):
+def selection(a, n):
     for i in range(n):
         min = i
         for j in range(i+1,n):
@@ -7,5 +7,5 @@ def insertion(a, n):
         a[i],a[min] = a[min],a[i]
 
 a = [4,6,9,2,1,5,7,4]
-insertion(a,len(a))
+selection(a,len(a))
 print(a)
